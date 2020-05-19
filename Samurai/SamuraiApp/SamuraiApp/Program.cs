7baac95f17433e4a0d4866a68f60fed3748d105b
tanviredu <ornobtanvir.git@gmail.com>
2020-05-20 02:48:45 +0600

    added

--- Samurai/SamuraiApp/SamuraiApp/Program.cs
+++ Samurai/SamuraiApp/SamuraiApp/Program.cs
@@ -13,31 +13,10 @@ namespace SamuraiApp
         static void Main(string[] args)
         {
 
-            GetSamurai();
-            AddSamurai();
-            GetSamurai();
+            
 
         }
 
-        private static void AddSamurai()
-        {
-            var db = new SamuraiContext();
-            var samurai = new Samurai
-            {
-                Name = "Tanvir rahman"
-            };
-            db.Add(samurai);
-            db.SaveChanges();
-        }
-
-        private static void GetSamurai()
-        {
-            var db = new SamuraiContext();
-            var query = from s in db.Samurais select s;
-            foreach (var s in query)
-            {
-                Console.WriteLine(s.Name);
-            }
-        }
+      
     }
 }
