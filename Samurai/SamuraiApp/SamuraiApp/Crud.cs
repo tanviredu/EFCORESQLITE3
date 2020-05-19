321a046d362b578850030ea9c22b264fe2e3082f
tanviredu <ornobtanvir.git@gmail.com>
2020-05-20 01:25:06 +0600

    added

---
+++ Samurai/SamuraiApp/SamuraiApp/Crud.cs
@@ -0,0 +1,51 @@
+﻿using SamuraiApp.Domain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SamuraiApp
+{
+    
+    // apply both functional and the query syntax to do that
+
+    
+    class Crud
+    {
+        public Samurai CreateSamurai()
+        {
+
+
+            return new Samurai();
+        }
+
+
+        public Samurai ReadSamurai()
+        {
+
+
+            return new Samurai();
+        }
+
+        public Samurai UpdateSamurai(Samurai samurai)
+        {
+
+
+            return new Samurai();
+        }
+
+
+        public void DeleteSamurai(Samurai samurai)
+        {
+
+
+            
+        }
+
+        public IEnumerable<Samurai> GetMultipleSamurai()
+        {
+            List<Samurai> samurais = new List<Samurai>();
+            return samurais;
+        }
+
+    }
+}
